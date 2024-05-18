2b5f916e7d1a2d8390c63115e7307f24215eb2fe
TiguiNadon <blais.guillaume76@gmail.com>
2024-05-18 10:28:52 -0400

    Adding menu scenes

---
+++ TopDownShooter/TopDownShooter/Scenes/main_menu.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://TopDownShooter/TopDownShooter/Scenes/target_practice.tscn")
