8f30711ceceffcfd74e9513980d19dc3c296197b
Munir <le2176022@crc-lennox.qc.ca>
2024-05-22 18:32:22 -0400

    Modified the UI scenes to have the correct button functionalities and moved some texts in the main_menu.tscn

--- TopDownShooter/TopDownShooter/Scenes/main_menu.gd
+++ TopDownShooter/TopDownShooter/Scenes/main_menu.gd
@@ -2,21 +2,9 @@ extends CanvasLayer
 
 var master_bus = AudioServer.get_bus_index("Master")
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://TopDownShooter/TopDownShooter/Scenes/target_practice.tscn")
 	
-
-
 func _on_main_menu_button_pressed():
 	get_tree().change_scene_to_file("res://TopDownShooter/TopDownShooter/Scenes/main_menu.tscn")
 	
@@ -28,10 +16,6 @@ func _on_quit_button_pressed():
 func _on_option_button_pressed():
 	get_tree().change_scene_to_file("res://TopDownShooter/TopDownShooter/Scenes/option_menu.tscn")
 
-
-
-
-
 func _on_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(master_bus, value)
 	
@@ -39,3 +23,4 @@ func _on_slider_value_changed(value):
 		AudioServer.set_bus_mute(master_bus, true)
 	else:
 		AudioServer.set_bus_mute(master_bus, false)
+
--- TopDownShooter/TopDownShooter/scripts/ui.gd
+++ TopDownShooter/TopDownShooter/scripts/ui.gd
@@ -4,19 +4,15 @@ var red_texture = preload("res://TopDownShooter/TopDownShooter/ui/progress_bar/p
 var yellow_texture = preload("res://TopDownShooter/TopDownShooter/ui/progress_bar/progress2.png")
 var green_texture = preload("res://TopDownShooter/TopDownShooter/ui/progress_bar/progress.png")
 var orange_texture = preload("res://TopDownShooter/TopDownShooter/ui/progress_bar/progress4.png")
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$TargetCounter/HBoxContainer/Label.text = "Kill:" + str(Globals.target_destroyed)
+	$TargetCounter/HBoxContainer/Label.text = "Kill: " + str(Globals.target_destroyed)
 	$grenadeUI/HBoxContainer/ProgressBar.value = Globals.reload_progress_grenade
 	$grenadeUI/HBoxContainer/TextureProgressBar.value = Globals.health
 	var xpPercentage = (Globals.playerXp * 100) / Globals.xpForNextLevel
 	$XpProgressBar.value = xpPercentage
-	$XpProgressBar/Level.text = str(Globals.playerLevel)
+	$XpProgressBar/WaveLevel.text = "Wave: " + str(Globals.playerLevel)
 	$grenadeUI/HBoxContainer/ProgressBar/AmmoCount.text = str(Globals.maxAmmo - Globals.ammoCount)
 	if Globals.health < 30:
 		$grenadeUI/HBoxContainer/TextureProgressBar.set_progress_texture(red_texture)
